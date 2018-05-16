e347ebc8dcf51d1b830b969eb0a175aac093adf8
BobinskiM <bobinski.mateusz@gmail.com>
2018-05-16 17:07:29 +0200

    Initial commit

---
+++ Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqldoc
+{
+    class Options
+    {
+        [Option('s', "script", HelpText = "Input file to be processed.")]
+        public string InputFile { get; set; }
+
+        [Option('d', "directory", HelpText = "Directory to search for scripts.")]
+        public string InputDir { get; set; }
+
+        [Option('t', "tables", Default = true, HelpText = "Documents tables.")]
+        public bool Tables { get; set; }
+
+        [Option('f', "filter", HelpText = "Filters for object names to be documented.")]
+        public IEnumerable<string> Filters { get; set; }
+
+        [Option('v', "verbose", Default = false, HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [Option('o', "output", HelpText = "Asciidoc output file.")]
+        public string Output { get; set; }
+
+
+    }
+}
