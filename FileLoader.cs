6bc1dc53372880bc37ef8ffccaba0acb200bc18a
BobinskiM <bobinski.mateusz@gmail.com>
2018-05-17 15:56:05 +0200

    opencover test

---
+++ FileLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlDoctor
+{
+    public class FileLoader
+    {
+        public IEnumerable<string> LoadFiles(string path, string filter, bool recursive)
+        {
+            return new List<string>();
+        }
+    }
+}
--- tests/Class1.cs
+++ tests/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlDoctor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,8 @@ namespace tests
         public void FailingTest()
         {
             Assert.Equal(5, Add(2, 2));
+            FileLoader loader = new FileLoader();
+            Assert.Empty(loader.LoadFiles(null, null, false));
         }
 
         int Add(int x, int y)
